04e6422d21a74e05a3cb253b7fefcc83364978ab
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2024-02-29 15:14:36 -0400

    atualizando models

--- scraper/models.py
+++ scraper/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 from django.db import models
 
-class ItemColetado(models.Model):
-    codigo = models.CharField(max_length=100)
-    descricao_servico = models.TextField()
-    unidade = models.CharField(max_length=50)
-    custo_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+class Edital(models.Model):
+    descricao_objeto = models.TextField()
+    modalidade = models.CharField(max_length=50)
+    comprador = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descricao_servico
+        return self.descricao_objeto
+
+class Itens(models.Model):
+    licitacao = models.ForeignKey(Edital, on_delete=models.CASCADE)
+    descricao = models.TextField()
+    unidade = models.CharField(max_length=50)
+    quantidade = models.IntegerField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
