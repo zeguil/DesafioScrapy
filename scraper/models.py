796c6d0e52330b5bcf0148212caaa42d8c503181
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2024-02-29 14:09:44 -0400

    add models

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class ItemColetado(models.Model):
+    codigo = models.CharField(max_length=100)
+    descricao_servico = models.TextField()
+    unidade = models.CharField(max_length=50)
+    custo_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.descricao_servico
